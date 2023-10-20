4ce6952de837dc3b52dff437a116ce04144050b9
Soares007 <soaresstar947@gmail.com>
2023-10-20 17:19:50 -0300

    Correção da Pasta sistema-escolar-backend

---
+++ sistema-escolar-backend/src/main/java/fatec/sp/gov/br/sistemaescolarbackend/SistemaEscolarBackendApplication.java
@@ -0,0 +1,13 @@
+package fatec.sp.gov.br.sistemaescolarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEscolarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEscolarBackendApplication.class, args);
+	}
+
+}
---
+++ sistema-escolar-backend/src/test/java/fatec/sp/gov/br/sistemaescolarbackend/SistemaEscolarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fatec.sp.gov.br.sistemaescolarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEscolarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
